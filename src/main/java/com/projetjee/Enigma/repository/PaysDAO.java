d165077324cd766a492709c32a355e8877a918c2
Ugolty <latry.ugo@gmail.com>
2023-10-26 22:22:15 +0200

    Repair

--- src/main/java/com/projetjee/Enigma/repository/PaysDAO.java
+++ src/main/java/com/projetjee/Enigma/repository/PaysDAO.java
@@ -1,2 +1,7 @@
-package com.projetjee.Enigma.repository;public interface PaysDAO {
+package com.projetjee.Enigma.repository;
+
+import com.projetjee.Enigma.models.Pays;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaysDAO extends JpaRepository<Pays, String> {
 }
