8766442f8ec45ed6bf7994e7cfbe0183ba418554
Ugolty <latry.ugo@gmail.com>
2023-11-03 00:40:02 +0100

    Update

--- src/main/java/com/projetjee/enigma/models/Pays.java
+++ src/main/java/com/projetjee/enigma/models/Pays.java
@@ -18,21 +18,9 @@ public class Pays {
     @ManyToOne
     @JoinColumn(name = "IdPoeme")
     private Poeme IdPoeme;
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "IdGeo")
     private Geoguesser IdGeo;
-    @Column(length = 10, name = "Q1")
-    private boolean Q1;
-    @Column(length = 10, name = "Q2")
-    private boolean Q2;
-    @Column(length = 10, name = "Q3")
-    private boolean Q3;
-    @Column(length = 10, name = "Q4")
-    private boolean Q4;
-    @Column(length = 10, name = "Q5")
-    private boolean Q5;
-    @Column(length = 10, name = "Q6")
-    private boolean Q6;
     @Column(length = 10, name = "NumVers")
     private int NumVers;
 }
