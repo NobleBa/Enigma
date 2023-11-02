b6368a89391590ebee8f244bc1a3af7bb91d1424
DEPRETER-Remi <depreterremi@gmail.com>
2023-11-02 23:36:37 +0100

    reglage raph poeme

--- src/main/java/com/projetjee/enigma/models/Poeme.java
+++ src/main/java/com/projetjee/enigma/models/Poeme.java
@@ -16,7 +16,7 @@ public class Poeme {
     @Id
     @Column(length = 50, name = "IdPoeme")
     private String IdPoeme;
-    @Column(length = 250, name = "Poeme")
+    @Column(length = 1000, name = "Poeme")
     private String poeme;
     @Column(length = 500, name = "Solution")
     private String Solution;
