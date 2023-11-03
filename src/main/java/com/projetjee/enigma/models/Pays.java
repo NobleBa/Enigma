fed87be807bc41c6cc173d440d580f063d69cdd5
Ugolty <latry.ugo@gmail.com>
2023-11-03 21:36:55 +0100

    Update Pays

--- src/main/java/com/projetjee/enigma/models/Pays.java
+++ src/main/java/com/projetjee/enigma/models/Pays.java
@@ -15,14 +15,21 @@ public class Pays {
     @Id
     @Column(length = 50, name = "IdPays")
     private String IdPays;
-    @Column(length = 50, name = "Nompays")
-    private String Nompays;
-    @ManyToOne
-    @JoinColumn(name = "IdPoeme")
-    private Poeme IdPoeme;
+    @Column(length = 50, name = "nompays")
+    private String nompays;
+    @Column(name = "continent")
+    private String continent;
+    @Column(name = "fine")
+    private boolean fine;
+    @Column(name = "littoral")
+    private boolean littoral;
+    @Column(name = "surfaceMillion")
+    private boolean surfaceMillion;
+    @Column(name = "populationHuitMillions")
+    private boolean populationHuitMillions;
+    @Column(name = "questionUnique")
+    private String questionUnique;
     @OneToOne
     @JoinColumn(name = "IdGeo")
     private Geoguesser IdGeo;
-    @Column(length = 10, name = "NumVers")
-    private int NumVers;
 }
--- src/main/java/com/projetjee/enigma/repository/PaysDAO.java
+++ src/main/java/com/projetjee/enigma/repository/PaysDAO.java
@@ -4,4 +4,10 @@ import com.projetjee.enigma.models.Pays;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaysDAO extends JpaRepository<Pays, String> {
+    boolean existsByNompaysAndContinent(String nompays, String continent);
+    boolean existsByNompaysAndContinentAndFine(String nompays, String continent, boolean fine);
+    boolean existsByNompaysAndContinentAndFineAndLittoral(String nompays, String continent, boolean fine, boolean littoral);
+    boolean existsByNompaysAndContinentAndFineAndLittoralAndPopulationHuitMillions(String nompays, String continent, boolean fine, boolean littoral, boolean population);
+    boolean existsByNompaysAndContinentAndFineAndLittoralAndPopulationHuitMillionsAndAndSurfaceMillion(String nompays, String continent, boolean fine, boolean littoral, boolean population, boolean surface);
+    boolean existsByNompaysAndContinentAndFineAndLittoralAndPopulationHuitMillionsAndAndSurfaceMillionAndQuestionUnique(String nompays, String continent, boolean fine, boolean littoral, boolean population, boolean surface, String unique);
 }
